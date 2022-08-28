ebda89dbe81e6979050607cf84223173e6eb0f8a
Chris McKnight <tenlongfingers10@gmail.com>
2022-08-27 18:25:28 -0600

    New demo files 8.27.22

--- JSObjects/02/demos/demos/m02-after/demo.js
+++ JSObjects/02/demos/demos/m02-after/demo.js
@@ -1,23 +1,30 @@
-'use strict'; 
-(function() {
+"use strict";
+(function () {
   // let person = {
   //   firstName: 'Jim',
   //   lastName: 'Cooper',
   //   age: 29,
   //   isAdult: function() { return this.age > 18; }
   // };
-  
+
   function Person(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
-    this.isAdult = function() { return this.age > 21; }
+    this.isAdult = function () {
+      return this.age > 21;
+    };
   }
 
-  let jim = new Person('Jim', 'Cooper', 29);
-  let sofia = new Person('Sofia', 'Cooper', 17);
+  let jim = new Person("Jim", "Cooper", 29);
+  let sofia = new Person("Sofia", "Cooper", 17);
 
   display(jim.isAdult());
   display(sofia.isAdult());
-
 })();
+
+//const Array = [10, 20, 30];
+
+// for (let value of Array) {
+//   value += 1;
+//   console.log(value);

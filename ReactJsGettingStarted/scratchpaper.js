39c203e22b12dfad787b5462569264d9afa6d235
Chris McKnight <tenlongfingers10@gmail.com>
2022-08-29 18:35:36 -0600

    8.29.22 1

--- ReactJsGettingStarted/scratchpaper.js
+++ ReactJsGettingStarted/scratchpaper.js
@@ -1,1 +1,7 @@
 document.getElementById("mountNode").innerHTML = "Hello!";
+
+function Hello() {
+  return <div>Hello React!</div>;
+}
+
+ReactDOM.render(<Hello />, document.getElementById("mountNode"));

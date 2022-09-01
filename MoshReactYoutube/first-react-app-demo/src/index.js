110c17aae574a23e1f9a5c02f01f23d7079e7978
Chris McKnight <tenlongfingers10@gmail.com>
2022-08-31 19:53:45 -0600

    8.31.22 1

--- MoshReactYoutube/first-react-app-demo/src/index.js
+++ MoshReactYoutube/first-react-app-demo/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+import Counter from "./components/counter";
 
 const element = <h1>Hello World</h1>;
-ReactDom.render(element, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById("root"));

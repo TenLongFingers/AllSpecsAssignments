eec1fb3cbb958fceb5c0d8f3c5b54d04ddadab17
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-03 17:20:18 -0600

    adding a method and using truthy statements

--- MoshReactYoutube/shopping-cart-demo/src/components/counter.jsx
+++ MoshReactYoutube/shopping-cart-demo/src/components/counter.jsx
@@ -7,33 +7,38 @@ class Counter extends Component {
     };
     
     styles ={
-        fontSize: 10,
+        fontSize: 14,
         fontWeight: "bold"
 
     }
-
-
+ 
+    
     render() { 
 return (
     <div>
+        {/* {this.state.tags.length === 0 && `Please create a new tag`} */}
+        {/* This works because if both statements are truthy (array length is zero and the existance of a non=empty string), the second operand will be returned. */}
         <span style={{fontSize : 18}}className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                { this.state.tags.map(tag => <li key = {tag}>{ tag } </li>)}
-            </ul>
+{this.renderTags()}
             </div>
             );
-    }
-    getBadgeClasses() {
-        let classes = "m-2 badge bg-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
+        }
+        getBadgeClasses() {
+            let classes = "m-2 badge bg-";
+            classes += (this.state.count === 0) ? "warning" : "primary";
+            return classes;
+        }
+        
+        formatCount () {
+            const {count} = this.state;
+            return this.state.count === 0 ? `Zero` : this.state.count;
+        }
 
-    formatCount () {
-        const {count} = this.state;
-        return this.state.count === 0 ? `Zero` : this.state.count;
+        renderTags(){
+            if (this.state.tags.length === 0) return <p>Your shopping cart is empty</p>;
+            return <ul> { this.state.tags.map(tag => <li key = {tag}>{ tag } </li>)}</ul>
+        }
     }
-}
- 
-export default Counter;
+    
+    export default Counter;

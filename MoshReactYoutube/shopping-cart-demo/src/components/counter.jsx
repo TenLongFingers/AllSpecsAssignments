519b763b3f5e75a3b600ed77e1a79b5eee8dfa7d
Chris McKnight <tenlongfingers10@gmail.com>
2022-09-03 16:46:11 -0600

    added badges

--- MoshReactYoutube/shopping-cart-demo/src/components/counter.jsx
+++ MoshReactYoutube/shopping-cart-demo/src/components/counter.jsx
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 class Counter extends Component {
     state = {
         count: 0,
-        imageURL: "https://picsum.photos/200"
     };
     
+    styles ={
+        fontSize: 10,
+        fontWeight: "bold"
+
+    }
+
+
     render() { 
+        let classes = "m-2 badge bg-";
+        classes += (this.state.count === 0) ? "warning" : "primary"
 return (
     <div>
-        <img src={this.state.imageURL} alt="random image"/>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+        <span style={{fontSize : 18}}className={classes}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
             );
     }
